674cb9e4d63563b3aeb673abe5e8c37e183b35a1
xxu141 <xxu141@myseneca.ca>
2024-07-31 23:10:18 -0400

    generate jwt token

--- pages/api/login/index.js
+++ pages/api/login/index.js
@@ -1,25 +1,42 @@
 /* eslint-disable import/no-anonymous-default-export */
 import clientPromise from "@/lib/mongodb";
+const jwt = require('jsonwebtoken');
+
+// Configure its options
+let jwtOptions = {
+  secretOrKey: '&0y7$noP#5rt99&GB%Pz7j2b1vkzaB0RKs%^N^0zOP89NT04mPuaM!&G8cbNZOtH',
+};
+
 
 export default async (req, res) => {
   try {
-    const { name } = req.body;
+    const { username, password } = req.body;
     const { method } = req;
     const client = await clientPromise;
     const db = client.db("shopping");
 
     switch (method) {
       case "POST":
-        // 1. check username and password (in db)
-        // const users = await db
-        //   .collection("users")
-        //   .find({})
-        //   .limit(10)
-        //   .toArray();
-        // res.json(users);
+        // 1. check if username exists in db
+        const user = await db
+          .collection("users")
+          .findOne({ username: username });
+      
+        if (!user) {
+          res.status(404).end(`User not found`);
+        } 
+        // 2. check if password matches
+        if (user.password != password) {
+          res.status(400).end(`Username and password do not match!`);
+        }
 
-        // 2. generate jwt
-        // 3. return jwt to fronend
+        // 3. Sign in user - generate JWT and return JWT to frontend
+        let payload = { 
+          _id: user._id,
+          username: user.username,
+        };
+        let token = jwt.sign(payload, jwtOptions.secretOrKey);
+        res.status(200).json({ "message": "login successful", "token": token });
 
         break;
       case "GET":
@@ -29,6 +46,8 @@ export default async (req, res) => {
           .limit(10)
           .toArray();
         res.json(users);
+
+        break;
       default:
         res.setHeader("Allow", ["POST"]);
         res.status(405).end(`Method ${method} Not Allowed`);
