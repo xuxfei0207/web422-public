3a463333d263dd3a23193b6d865a037cf9598a00
xxu141 <xxu141@myseneca.ca>
2024-07-31 23:43:20 -0400

    finish login flow

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,10 +2,21 @@ import React from "react";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import { useCart } from "../context/CartContext";
+import { getToken } from "@/lib/authenticate";
+import { useEffect, useState } from 'react';
 
 const Navbar = ({ handleSearch }) => {
   const { state } = useCart();
+  const [jwtToken, setJwtToken] = useState([]);
+  
+  useEffect(() => {
+    const access_token = localStorage.getItem('access_token');
+    console.log("access_token: ", access_token);
+    setJwtToken(access_token);
+  }, []);
+
   console.log("Navbar state: ", state);
+  
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
@@ -51,39 +62,23 @@ const Navbar = ({ handleSearch }) => {
             </Link>
           </li>
         </ul>
-        <ul className={styles.navMenu}>
+        {jwtToken ? (
+          <ul className={styles.navMenu}>
+            <li className={styles.navItem}>
+              <Link href="/logout" className={styles.navLinks}>
+                Log out
+              </Link>
+            </li>
+          </ul>
+        ) : (
+          <ul className={styles.navMenu}>
             <li className={styles.navItem}>
               <Link href="/login" className={styles.navLinks}>
                 Login
               </Link>
             </li>
-        </ul>
-        {/* <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-          <form className="d-flex" onSubmit={handleSearch}>
-            <input
-              className="form-control me-2"
-              type="search"
-              name="search"
-              placeholder="Product ID (Number)"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div> */}
+          </ul>
+        )}
       </div>
     </nav>
   );
